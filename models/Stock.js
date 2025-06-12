ce23dd666307aea8bac4e8287f3bc9dcf4f3d4a4
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-06-12 09:00:03 -0300

    Delete models/Stock.js

--- models/Stock.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const stockSchema = new mongoose.Schema({
-  stock: String,
-  likes: [String],
-});
-
-module.exports = mongoose.model('Stock', stockSchema);
