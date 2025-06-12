1fe18849e9c1fd66a67e6c4e2d59d39711bcecb0
Silvanagb <104772778+Silvanagb@users.noreply.github.com>
2025-06-12 08:28:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 const apiRoutes = require('./routes/api.js');
@@ -11,13 +10,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
 
 module.exports = app;
